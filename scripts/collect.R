dcb6a86a05a3340ee66f35a8cdaa27469b5a80ff
Joel Day <dayjoel@gmail.com>
2016-03-03 12:58:59 -0800

    Filled out tags.

--- scripts/collect.R
+++ scripts/collect.R
@@ -1,8 +1,14 @@
 # collect.R
 
 library(streamR)
+library(twitteR)
+
+setwd("~/src/INFO_498F/final/INFO498_final_project")
 source("scripts/tags.R")
+source("scripts/tags_historical.R")
+source("scripts/credentials.R")
 load("my_oauth.Rdata")
+
 
 collect_tweets <- function(tags) {
   filterStream(file.name = "tweets.json",
--- scripts/tags.R
+++ scripts/tags.R
@@ -2,18 +2,45 @@
 
 TAGS_BERNIE <- c(
   "Bernie Sanders",
-  "Bernie",
-  "Sanders",
+  "#BernieSanders",
   "Feel The Bern",
-  "FeelTheBern",
+  "#FeelTheBern",
   "Free College",
   "Free Healthcare",
-  "Bernie2016"
+  "#Bernie2016"
 )
 
 TAGS_HILLARY <- c(
   "hillary clinton",
-  "hillary",
-  "clinton",
-  "secretary of state"
+  "secretary of state",
+  "Hillary for America",
+  "#HillaryForAmerica",
+  "#Clinton2016"
+)
+
+TAGS_TRUMP <- c(
+  "Donald Trump",
+  "Drumpf",
+  "Make America Great Again",
+  "RealDonaldTrump",
+  "#Trump2016"
+)
+
+TAGS_CARSON <- c(
+  "Ben Carson",
+  "Heal. Inspire. Revive.",
+  "#HealInspireRevive",
+  "#Carson2016"
+)
+
+TAGS_CRUZ <- c(
+  "Ted Cruz",
+  "Reigniting the Promise of America",
+  "#Cruz2016"
+)
+
+TAGS_RUBIO <- c(
+  "Marco Rubio",
+  "A New American Century",
+  "#Rubio2016"
 )
