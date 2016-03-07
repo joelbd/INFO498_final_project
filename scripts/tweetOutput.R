e4ec2f15d8a4578f5403d8f87684ebced080c21a
Joel Day <dayjoel@gmail.com>
2016-03-06 21:45:06 -0800

    Adding tweet parser code.

--- scripts/tweetOutput.R
+++ scripts/tweetOutput.R
@@ -37,16 +37,18 @@ runApp(list(
   server = function(input, output, session) {
     
     gotweet <- eventReactive(input$update, {
-      collect_tweets(eval(parse(text = input$candidate)), 10)
+      withProgress(message = 'Fetching tweets.', value = 0, {
+          collect_tweets(eval(parse(text = input$candidate)), 1)
+      })
     })
     
     autoInvalidate <- reactiveTimer(10000, session)
     
     output$tweetTable <- renderDataTable({
       gotweet()
-      autoInvalidate()
+      # autoInvalidate()
       filter_tweets()
-    }, options = list(ordering = FALSE)
+    }, options = list(ordering = FALSE, searching = FALSE)
     )
   }
 ))
