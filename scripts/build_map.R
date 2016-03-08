6494d775a4aa610226ee6881c38347601784601d
Sarah <sarah.g.stanton@gmail.com>
2016-03-08 15:32:53 -0800

    build_map

---
+++ scripts/build_map.R
@@ -0,0 +1,22 @@
+# build_map
+
+library(dplyr)
+library(plotly)
+
+build_map <- function(data) {
+  g <- list(
+    scope = 'usa',
+    projection = list(type = 'albers usa'),
+    showland = TRUE,
+    landcolor = toRGB("gray85"),
+    subunitwidth = 1,
+    countrywidth = 1,
+    subunitcolor = toRGB("white"),
+    countrycolor = toRGB("white")
+  )
+  
+  plot_ly(data, lat=latitude, lon=longitude, text=text, mode='markers', marker = 
+            list(size = 7, symbol = 'circle', opacity = 0.5), hoverinfo = 'none', type="scattergeo", 
+          locationmode='USA-states') %>% layout(geo=g)
+}
+
