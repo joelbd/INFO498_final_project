e32c30da4d791c02b75a1b7ccafc921a28e4c450
Sarah <sarah.g.stanton@gmail.com>
2016-03-08 17:06:09 -0800

    commented code, renamed function

--- scripts/build_map.R
+++ scripts/build_map.R
@@ -3,8 +3,11 @@
 library(dplyr)
 library(plotly)
 
+#function to build map takes csv file as parameter
 build_map <- function(file) {
+  #read in data from file
   data <- read.csv(file, stringsAsFactors = FALSE)
+  #setup geo plotly parameter
   g <- list(
     scope = 'usa',
     projection = list(type = 'albers usa'),
@@ -15,7 +18,7 @@ build_map <- function(file) {
     subunitcolor = toRGB("white"),
     countrycolor = toRGB("white"),
   )
-  
+  #plot data using longitude and latitude data columns
   plot_ly(data, lat=latitude, lon=longitude, text=text, mode='markers', marker = 
             list(size = 7, symbol = 'circle', opacity = 0.5), hoverinfo = 'none', type="scattergeo", 
           locationmode='USA-states') %>% layout(geo=g)
