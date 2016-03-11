704e4cf38a3aeb32cb5bcbec2bef5b673fc81e45
Joel Day <dayjoel@gmail.com>
2016-03-11 12:24:12 -0800

    Added comments to OAuth.R

--- scripts/oauth.R
+++ scripts/oauth.R
@@ -4,18 +4,21 @@ library(ROAuth)
 
 source("scripts/credentials.R")
 
+# Define twitter API urls
 requestURL <- "https://api.twitter.com/oauth/request_token"
 accessURL <- "https://api.twitter.com/oauth/access_token"
 authURL <- "https://api.twitter.com/oauth/authorize"
 consumerKey <- API_KEY
 consumerSecret <- API_SECRET
 
+# Define global OAuth.
 my_oauth <- OAuthFactory$new(consumerKey = consumerKey,
                              consumerSecret = consumerSecret,
                              requestURL = requestURL,
                              accessURL = accessURL,
                              authURL = authURL)
 
+# Shake hands
 my_oauth$handshake(cainfo = system.file("CurlSSL", "cacert.pem", package = "RCurl"))
 
 save(my_oauth, file = "my_oauth.Rdata")
